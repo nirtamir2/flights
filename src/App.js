f337971ddaf12f038b9151d851a644455c2a6d34
Nir Tamir <nirtamir2@gmail.com>
2018-08-06 12:28:39 +0300

    add flight component and navigate to it when user is logged in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import Login from './components/Login/Login'
-import {BrowserRouter} from 'react-router-dom'
+import Flights from './components/Flights/Flights'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter className="App">
-                <Login/>
+                <Switch>
+                    <Route path="/flights" component={Flights}/>
+                    <Route path="/" component={Login}/>
+                </Switch>
             </BrowserRouter>
         );
     }
---
+++ src/components/Flights/Flights.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class Flights extends Component {
+    state = {
+
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>flights</h1>
+            </div>
+        )
+    }
+}
+
+export default Flights;
