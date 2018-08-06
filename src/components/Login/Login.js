8ea86414a29506ed687bc9a7a9e0e7a48d90037f
Nir Tamir <nirtamir2@gmail.com>
2018-08-06 12:06:49 +0300

    create login component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+
+class Login extends Component {
+    state = {
+        username: '',
+        password: '',
+        error: null
+    }
+
+    login = (e) => {
+        e.preventDefault()
+        const isValidLogin = this.state.username === 'user' && this.state.password === 'password'
+        if (!isValidLogin) {
+            this.setState({error: 'Wrong user or password'})
+        }
+        else {
+            console.log('login')
+        }
+    }
+
+    setUserName = (e) => {
+        this.setState({username: e.target.value})
+    }
+
+    setPassword = (e) => {
+        this.setState({password: e.target.value})
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.login}>
+                <input
+                    type="text"
+                    placeholder="user"
+                    value={this.state.username}
+                    onChange={this.setUserName}/>
+                <input type="password"
+                       placeholder="password"
+                       value={this.state.password}
+                       onChange={this.setPassword}
+                />
+                <button type="submit">Login</button>
+                {this.state.error ? <span>{this.state.error}</span> : null}
+            </form>
+        )
+    }
+}
+
+export default Login;
